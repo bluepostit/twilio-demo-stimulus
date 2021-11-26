3e046fb82f29d00762c3e367336bc61626330acc
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-27 00:03:22 +0100

    model, route, ctlr + view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.others(current_user) if user_signed_in?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 1 }
+
+  def self.others(user)
+    User.where.not(id: user.id)
+  end
 end
