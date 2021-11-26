c45434bfda67bf49f3db247685f6e54ce12c465b
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-27 00:39:09 +0100

    add 3p deps, old html

---
+++ app/services/twilio_service.rb
@@ -0,0 +1,55 @@
+require 'twilio-ruby'
+
+class TwilioService
+  # Generate a token for user1 to chat to user2
+
+  def initialize
+    # Required for any Twilio Access Token
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    # To create an API key & secret for your app, go to
+    #  https://www.twilio.com/console/project/api-keys
+    @api_key = ENV['TWILIO_API_KEY']
+    @api_secret = ENV['TWILIO_API_SECRET']
+  end
+
+  def generate_token(user1, user2)
+    # Identify user1
+    identity = "user-#{user1.id}"
+
+    # Identify the video room
+    room = unique_room_id(user1, user2)
+
+    # Create video grant for the token
+    grant = Twilio::JWT::AccessToken::VideoGrant.new
+    grant.room = room
+
+    token = build_token(grant, identity)
+
+    # Generate JWT from token
+    jwt = token.to_jwt
+
+    {
+      token: jwt,
+      room: room
+    }
+  end
+
+  def unique_room_id(user1, user2)
+    id = [user1, user2].map(&:id).sort.join('-')
+    "room-#{id}"
+  end
+
+  private
+
+  def build_token(grant, identity)
+    # Create a Twilio access token
+    # See https://www.twilio.com/docs/video/tutorials/user-identity-access-tokens#generating-access-tokens
+    Twilio::JWT::AccessToken.new(
+      @account_sid,
+      @api_key,
+      @api_secret,
+      [grant],
+      identity: identity
+    )
+  end
+end
