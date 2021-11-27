998af90de9e5d261f3706f01f72e3f13d2764dcc
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-27 22:10:33 +0100

    restructure helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     return if @user == current_user
 
     twilio = TwilioService.new
-    @video_call_token = twilio.generate_token(current_user, @user)
+    twilio.generate_token(current_user, @user)
+    @twilio_jwt = twilio.jwt
+    @room_id = twilio.room_id
   end
 end
--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
 
 class TwilioService
-  # Generate a token for user1 to chat to user2
+  attr_reader :jwt, :room_id
 
   def initialize
     # Required for any Twilio Access Token
@@ -12,26 +12,20 @@ class TwilioService
     @api_secret = ENV['TWILIO_API_SECRET']
   end
 
+  # Generate a token for user1 to chat to user2
   def generate_token(user1, user2)
     # Identify user1
     identity = "user-#{user1.id}"
 
     # Identify the video room
-    room = unique_room_id(user1, user2)
+    @room_id = unique_room_id(user1, user2)
 
     # Create video grant for the token
     grant = Twilio::JWT::AccessToken::VideoGrant.new
-    grant.room = room
+    grant.room = @room_id
 
     token = build_token(grant, identity)
-
-    # Generate JWT from token
-    jwt = token.to_jwt
-
-    {
-      token: jwt,
-      room: room
-    }
+    @jwt = token.to_jwt
   end
 
   def unique_room_id(user1, user2)
